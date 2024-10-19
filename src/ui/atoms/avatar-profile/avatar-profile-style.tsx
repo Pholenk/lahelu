444562c108b6ea4cae7a653d76891016436b4043
pholenk <pholenkadi17@gmail.com>
2024-10-19 17:59:38 +0700

    create avatar profile component

---
+++ src/ui/atoms/avatar-profile/avatar-profile-style.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const AvatarProfileStyles = StyleSheet.create({
+  container: {
+    borderRadius: 20,
+  },
+  image: {
+    height: 30,
+    width: 30,
+    borderRadius: 20,
+  },
+});
